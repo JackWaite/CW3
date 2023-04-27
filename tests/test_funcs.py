1a733db2cbfe1ab0afe68ae7522c0c647f5940ad
Jack Waite <hardgimbarrforever@gmail.com>
2023-04-27 17:28:15 +0300

    Fix tests

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,38 +1,44 @@
+import pytest
+
 from utils import funcs
 import requests
-import pytest
 
 
 @pytest.fixture
 def words():
     response = requests.get("https://api.npoint.io/094d3a7a0d9569218a14")
-    words = response.json()
-    return words
+    words_1 = response.json()
+    return words_1
 
 
 def test_get_data(words):
-    assert type(funcs.get_data(words)) == type(words)
-    assert funcs.get_data(words) is not None
+    """Test function get_data"""
+    assert type(funcs.get_data()) == type(words)
+    assert funcs.get_data() is not None
 
 
 def test_executed_operations(words):
-    assert isinstance(funcs.executed_operations(words), list)
-    assert funcs.executed_operations(words) is not None
+    """Test function executed_operations"""
+    assert isinstance(funcs.executed_operations(), list)
+    assert funcs.executed_operations() is not None
 
 
-def test_sorted_data(words):
-    executed_operations = funcs.executed_operations(words)
-    assert isinstance(funcs.sorted_data(executed_operations), list)
-    assert funcs.sorted_data(executed_operations) != executed_operations
+def test_sort_data(words):
+    """Test function sort_data"""
+    executed_operations = funcs.executed_operations()
+    assert isinstance(funcs.sort_data(), list)
+    assert funcs.sort_data() != executed_operations
 
 
 def test_last_operations():
-    sorted = funcs.last_operations()
-    assert len(sorted[:5]) == 5
-    assert isinstance(sorted[:5], list)
+    """Test function five last operations"""
+    sort_ed = funcs.last_operations()
+    assert len(sort_ed[:5]) == 5
+    assert isinstance(sort_ed[:5], list)
 
 
 def test_hiding_card():
+    """Test hiding_card"""
     last_five_operations = funcs.last_operations()[:5]
     assert isinstance(funcs.hiding_card(last_five_operations), list)
     for k in last_five_operations:
@@ -40,10 +46,12 @@ def test_hiding_card():
 
 
 def test_date_new():
+    """Test date_new"""
     last_five_operations = funcs.last_operations()[:5]
     assert isinstance(funcs.date_new(last_five_operations), list)
 
 
 def test_result_output():
+    """Test result_output"""
     last_five_operations = funcs.last_operations()[:5]
-    assert funcs.result_output(last_five_operations, None) is True
+    assert funcs.result_output(last_five_operations) is True
